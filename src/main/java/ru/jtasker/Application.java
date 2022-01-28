13a9a3bf2795f86a3776c7cafc413ec7d3a02924
Robert Imangulov <robby300im@gmail.com>
2022-01-29 00:58:37 +0500

    add builder pattern in ToDo

--- src/main/java/ru/jtasker/Application.java
+++ src/main/java/ru/jtasker/Application.java
@@ -1,12 +1,24 @@
 package ru.jtasker;
 
 
+import ru.jtasker.domain.ToDo;
 import ru.jtasker.domain.User;
+
+import java.time.LocalDateTime;
 
 public class Application {
 
     public static void main(String[] args) {
         User user = new User.Builder().id(1L).userName("Vasya").email("Sobaka@mail.ru").password("123").build();
+        ToDo toDo = new ToDo.Builder()
+                .id(2L)
+                .userId(1L)
+                .name("выспасться")
+                .description("Необходиммо выспаться")
+                .createdOn(LocalDateTime.now())
+                .deadline(LocalDateTime.MAX)
+                .isDone(false)
+                .parentToDoId(3).build();
         int i = 0;
     }
 
