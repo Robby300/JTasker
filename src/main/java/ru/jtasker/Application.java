0013dc42684677b253e4be33ac1ae3bfda0cb0a6
Robert Imangulov <robby300im@gmail.com>
2022-01-28 22:49:16 +0500

    add ToDo in domain package

--- src/main/java/ru/jtasker/Application.java
+++ src/main/java/ru/jtasker/Application.java
@@ -2,9 +2,17 @@ package ru.jtasker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
+    DataSource dataSource = new DriverManagerDataSource();
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
