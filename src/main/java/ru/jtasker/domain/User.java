635c6809d2d03333b92ae1447273f69f666c1a76
Robert Imangulov <robby300im@gmail.com>
2022-01-28 22:29:52 +0500

    add User in domain package

---
+++ src/main/java/ru/jtasker/domain/User.java
@@ -0,0 +1,21 @@
+package ru.jtasker.domain;
+
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String userName;
+    private String password;
+    private String email;
+}
