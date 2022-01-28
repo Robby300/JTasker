e55af5f0d56a5dd3297fb4edc8514f6438f739a0
Robert Imangulov <robby300im@gmail.com>
2022-01-28 23:15:11 +0500

    add methods

--- src/main/java/ru/jtasker/repository/ToDoesRepository.java
+++ src/main/java/ru/jtasker/repository/ToDoesRepository.java
@@ -1,4 +1,14 @@
 package ru.jtasker.repository;
 
+import ru.jtasker.domain.ToDo;
+import ru.jtasker.domain.User;
+
+import java.util.List;
+
 public interface ToDoesRepository {
+    List<ToDo> findAll();
+
+    void save(ToDo toDo);
+
+    List<ToDo> findByUser();
 }
--- src/main/java/ru/jtasker/repository/UsersRepository.java
+++ src/main/java/ru/jtasker/repository/UsersRepository.java
@@ -1,4 +1,9 @@
 package ru.jtasker.repository;
 
+import ru.jtasker.domain.User;
+
+import java.util.List;
+
 public interface UsersRepository {
+    void save(User user);
 }
