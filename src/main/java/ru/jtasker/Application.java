949d3fe2994c0bdbd3526c89587600f95a64e835
Robert Imangulov <robby300im@gmail.com>
2022-01-29 17:00:25 +0500

    Inject AppContext

--- src/main/java/ru/jtasker/Application.java
+++ src/main/java/ru/jtasker/Application.java
@@ -1,25 +1,32 @@
 package ru.jtasker;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+import ru.jtasker.config.AppConfig;
+import ru.jtasker.repository.ToDoesRepositoryImpl;
 import ru.jtasker.repository.db.ToDoTable;
 import ru.jtasker.repository.db.UserTable;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
+@Component
 public class Application {
+    ToDoesRepositoryImpl toDoesRepository;
     public static final String DB_URL = "jdbc:h2:mem:/";
     // Таблицы
     UserTable userTable;
     ToDoTable toDoTable;
     //ToDoesRepositoryImpl toDoesRepository;
 
-    public Application() throws SQLException {
+    public Application(ToDoesRepositoryImpl toDoesRepository) throws SQLException {
         userTable = new UserTable();
         toDoTable = new ToDoTable();
+        this.toDoesRepository = toDoesRepository;
     }
 
     // соединение с БД
@@ -41,7 +48,7 @@ public class Application {
         userTable.insert("INSERT INTO Users(id, username, password, email) VALUES (1, 'Ivan', '123', 'Ivan@mail.ru')");
     }
 
-    public void insertCommand(Scanner scanner) {
+    public void insertCommandForRegisteredUser(Scanner scanner) {
         String command = scanner.nextLine();
         switch (command) {
             case "1":
@@ -64,13 +71,14 @@ public class Application {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Scanner scanner = new Scanner(System.in);
-        Application application = new Application();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Application application = context.getBean(Application.class);
         application.createTablesAndForeignKeys();
         application.insertDefaultUser();
         System.out.println("Hello Ivan, tell me about your tasks");
         while (true) {
             application.printConsoleInterface();
-            application.insertCommand(scanner);
+            application.insertCommandForRegisteredUser(scanner);
         }
     }
 
---
+++ src/main/java/ru/jtasker/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.jtasker.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.jtasker")
+public class AppConfig {
+}
