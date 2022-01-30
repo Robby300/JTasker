0b218128b82dc29e939ab0b40335c10410b17670
Robert Imangulov <robby300im@gmail.com>
2022-01-30 21:30:56 +0500

    Fixes and optimized imports

--- src/main/java/ru/jtasker/ui/ToDoInterface.java
+++ src/main/java/ru/jtasker/ui/ToDoInterface.java
@@ -8,6 +8,8 @@ import ru.jtasker.repository.UsersRepository;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @Component
@@ -107,30 +109,15 @@ public class ToDoInterface {
     private void createAndSaveToDo(Scanner scanner, long parentId) throws SQLException {
         long userId = usersRepository.getCurrentUser().getId();
         System.out.println("Вы выбрали создание задачи:");
-        System.out.println("Введите наименование задачи:");
-        String name = scanner.nextLine();
-        System.out.println("Введите содержание задачи:");
-        String description = scanner.nextLine();
-        System.out.println("Введите дедлайн задачи:");
-        System.out.println("год YYYY:");
-        int year = Integer.parseInt(scanner.nextLine());
-        System.out.println("месяц mm:");
-        int month = Integer.parseInt(scanner.nextLine());
-        System.out.println("день месяца dd:");
-        int day = Integer.parseInt(scanner.nextLine());
-        System.out.println("час hh:");
-        int hour = Integer.parseInt(scanner.nextLine());
-        System.out.println("минут mm:");
-        int minute = Integer.parseInt(scanner.nextLine());
-        LocalDateTime deadline = LocalDateTime.of(year, month, day, hour, minute);
+        Object[] toDoParts = getToDoParts(scanner);
         ToDo toDo = new ToDo.Builder()
-                .name(name)
+                .name((String) toDoParts[0])
                 .userId(userId)
                 .parentToDoId(parentId)
-                .description(description)
+                .description((String) toDoParts[1])
                 .createdOn(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES))
                 .isDone(false)
-                .deadline(deadline)
+                .deadline((LocalDateTime) toDoParts[2])
                 .build();
         toDoesRepository.save(toDo);
         System.out.println("Задача успешно сохранена!");
@@ -139,9 +126,21 @@ public class ToDoInterface {
     private void editToDo(Scanner scanner, ToDo toDo) {
         System.out.println("Вы выбрали редактирование задачи:");
         System.out.println("Введите новое наименование задачи:");
+        Object[] toDoParts = getToDoParts(scanner);
+        toDo.setName((String) toDoParts[0]);
+        toDo.setDescription((String) toDoParts[1]);
+        toDo.setDeadline((LocalDateTime) toDoParts[2]);
+        toDoesRepository.editToDo(toDo);
+    }
+
+    public Object[] getToDoParts(Scanner scanner) {
+        Object[] toDoParts = new Object[3];
+        System.out.println("Введите имя задачи:");
         String name = scanner.nextLine();
+        toDoParts[0] = name;
         System.out.println("Введите содержание задачи:");
         String description = scanner.nextLine();
+        toDoParts[1] = description;
         System.out.println("Введите дедлайн задачи:");
         System.out.println("год YYYY:");
         int year = Integer.parseInt(scanner.nextLine());
@@ -154,10 +153,8 @@ public class ToDoInterface {
         System.out.println("минут mm:");
         int minute = Integer.parseInt(scanner.nextLine());
         LocalDateTime deadline = LocalDateTime.of(year, month, day, hour, minute);
-        toDo.setName(name);
-        toDo.setDescription(description);
-        toDo.setDeadline(deadline);
-        toDoesRepository.editToDo(toDo);
+        toDoParts[2] = deadline;
+        return toDoParts;
     }
 }
 
